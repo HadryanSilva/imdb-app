84d8aa675a31d78b29a040fe52e3418742d34deb
HadryanSilva <hadryan2001@gmail.com>
2023-02-10 14:05:33 -0300

    Repositorio Inicial do projeto

---
+++ src/main/java/br/com/hadryan/imdbapp/ImdbAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.imdbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbAppApplication.class, args);
+	}
+
+}
